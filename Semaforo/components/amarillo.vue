66ceebf249894dea01dc5bc93fdf6acbd82025f4
JESULIKE <jgonzalezhernandez41@correo.unicordoba.edu.co>
2024-11-05 00:37:49 -0500

    Add files via upload

---
+++ Semaforo/components/amarillo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="amarillo" :class="{ 'onamarillo': isOn }"></div>
+</template>
+
+<script>
+export default {
+  props: ['isOn']
+}
+</script>
+  
+<style>
+.amarillo {
+  position: absolute;
+  top: 56%;
+  left: 77.7%;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: yellow;
+
+}
+
+.onamarillo {
+  background-color: rgba(255, 255, 0, 0.2);
+}
+</style>
---
+++ Semaforo/pages/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="traffic-light">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" />
+
+    <rojo :isOn="isRedLightOn" />
+    <amarillo :isOn="isYellowLightOn" />
+    <verde :isOn="isGreenLightOn" />
+
+    <div class="button-container d-flex justify-content-center align-items-center flex-column">
+      <button type="button" class="btn btn-danger mb-3" @click="Cambiar">Alto</button>
+      <button type="button" class="btn btn-warning mb-3" @click="Cambiar2">Precaución</button>
+      <button type="button" class="btn btn-success mb-3" @click="Cambiar3">Puede Continuar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import rojo from './components/rojo.vue';
+import amarillo from './components/amarillo.vue';
+import verde from './components/verde.vue';
+
+export default {
+  components: {
+    rojo,
+    amarillo,
+    verde,
+  },
+  data() {
+    return {
+      isRedLightOn: true,
+      isYellowLightOn: true,
+      isGreenLightOn: true,
+    };
+  },
+  methods: {
+    Cambiar() {
+      this.isRedLightOn = false;
+      this.isYellowLightOn = true;
+      this.isGreenLightOn = true;
+    },
+    Cambiar2() {
+      this.isYellowLightOn = false;
+      this.isRedLightOn = true;
+      this.isGreenLightOn = true;
+    },
+    Cambiar3() {
+      this.isGreenLightOn = false;
+      this.isYellowLightOn = true;
+      this.isRedLightOn = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.traffic-light {
+  background-image: url('/public/fondo.jpg');
+  background-size: cover;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+</style>
