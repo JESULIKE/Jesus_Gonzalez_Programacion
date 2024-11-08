32d6798f8e45f81cc953869af46fd010a832d519
JESULIKE <jgonzalezhernandez41@correo.unicordoba.edu.co>
2024-11-08 00:43:26 -0500

    Add files via upload

---
+++ Tienda/components/carrito.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="carrito">
+        <div class="carr">
+            <v-badge color="error" :content="this.$store.getters.compras">
+                
+                    <v-btn icon @click="detalle()">
+                    <v-icon>mdi-cart</v-icon>
+                    </v-btn>
+            </v-badge>
+        </div>
+        <factura :show="show" @cerrar="cerrar"></factura>
+    </div>
+</template>
+<script>
+
+export default {
+    props: {
+
+
+    },
+    data() {
+        return {
+            show: false
+        }
+    },
+    methods: {
+        detalle() {
+            this.show = true
+        },
+
+        cerrar() {
+            this.show = false
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+.carrito {
+    background-color: cadetblue;
+    width: 100px;
+    height: 150px;
+}
+
+.carr {
+    margin-top: 50px;
+    margin-left: 25px;
+}
+</style>
